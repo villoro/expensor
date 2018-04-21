0c005f47a422f03e774a0a8594b8ec2405fb78a1
Villoro <villoro7@gmail.com>
2018-04-21 20:45:41 +0200

    [NEW] App violins implemented

--- src/app/pages/app_violins.py
+++ src/app/pages/app_violins.py
@@ -9,12 +9,53 @@ import utilities as u
 import constants as c
 from app import layout
 from dash_app import DFG, CATEGORIES, APP
-from plots import plots_heatmaps as plots
+from plots import plots_violins as plots
 
 
 CONTENT = [
+    layout.get_body_elem(
+        dcc.Graph(
+            id="violin_year", config=layout.PLOT_CONFIG,
+            figure=plots.violin_plot(DFG, c.cols.YEAR)
+        )
+    ),
+    layout.get_body_elem(
+        dcc.Graph(
+            id="violin_month", config=layout.PLOT_CONFIG,
+            figure=plots.violin_plot(DFG, c.cols.MONTH)
+        )
+    ),
 ]
 
 SIDEBAR = layout.create_sidebar(
     CATEGORIES,
 )
+
+@APP.callback(Output("violin_year", "figure"),
+              [Input("category", "value")])
+def update_heatmap_i(categories):
+    """
+        Updates the violin year plot
+
+        Args:
+            categories: categories to use
+    """
+
+    df = u.dfs.filter_data(DFG, categories)
+
+    return plots.violin_plot(df, c.cols.YEAR)
+
+
+@APP.callback(Output("violin_month", "figure"),
+              [Input("category", "value")])
+def update_heatmap_i(categories):
+    """
+        Updates the violin year plot
+
+        Args:
+            categories: categories to use
+    """
+
+    df = u.dfs.filter_data(DFG, categories)
+
+    return plots.violin_plot(df, c.cols.MONTH)
