cc2d39f2a6845b54820be4fe38afc7fea33cc403
Villoro <villoro7@gmail.com>
2018-07-29 18:20:45 +0200

    [NEW] Add totals in plots

--- src/plots/plots_liquid.py
+++ src/plots/plots_liquid.py
@@ -22,12 +22,9 @@ def liquid_plot(df_liq_in, df_list):
 
     df_liq = df_liq_in.set_index(c.cols.DATE)
 
-    if c.names.TOTAL in df_liq.columns:
-        del df_liq[c.names.TOTAL]
+    data = [go.Scatter(x=df_liq.index, y=df_liq[c.names.TOTAL],
+                       marker={"color": "black"}, name=c.names.TOTAL)]
 
-    df_liq[c.names.TOTAL] = df_liq.sum(axis=1)
-
-    data = [go.Scatter(x=df_liq.index, y=df_liq["Total"], marker={"color": "black"}, name="Total")]
     for level in df_list[c.cols.LIQUID_LEVEL].unique():
         df_aux = df_list[df_list[c.cols.LIQUID_LEVEL] == level]
         name_liq = df_aux[c.cols.LIQUID_NAME].tolist()[0]
