1f9cc28c9defa938eed080cc2af5654c4a3906ae
Villoro <villoro7@gmail.com>
2018-08-01 19:12:45 +0200

    [NEW] switch scatter plots to only lines mode

--- src/app/pages/app_liquid.py
+++ src/app/pages/app_liquid.py
@@ -3,12 +3,10 @@
 """
 
 import dash_core_components as dcc
-import dash_html_components as html
 from dash.dependencies import Input, Output
 
 import utilities as u
 import constants as c
-from static import styles
 from app import ui_utils as uiu
 from plots import plots_liquid as plots
 
