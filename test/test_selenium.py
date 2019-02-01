f92fa876c1528f0559f0dd5c14780522fb27c7ac
Villoro <villoro7@gmail.com>
2019-02-01 21:29:52 +0100

    [TST] fix test selenium

--- test/test_selenium.py
+++ test/test_selenium.py
@@ -22,9 +22,9 @@ class TestApp(unittest.TestCase):
             self.driver.get(c.dash.LINK_ROOT + page)
             wait_until_loaded(self.driver)
 
-            # Check that there is a sidebar and a body
-            self.driver.find_element_by_id("sidebar")
+            # Check that there is a body and filters divs
             self.driver.find_element_by_id("body")
+            self.driver.find_element_by_id("filters")
 
 
     def _check_one_page(self, page_link, elements):
@@ -53,33 +53,36 @@ class TestApp(unittest.TestCase):
     def test_page_evolution(self):
         """ Test the content of page evolution """
 
-        body_elem = ["plot_evol", "radio_evol_type", "drop_evol_categ", "radio_evol_tw"]
-        sidebar_elem = ["drop_evol_categ", "radio_evol_tw"]
-        self._check_one_page(c.dash.LINK_EVOLUTION, body_elem + sidebar_elem)
+        contents = ["plot_evol", "plot_evo_detail", "radio_evol_type"]
+        filters = ["input_categories", "input_smoothing", "input_timewindow"]
+        self._check_one_page(c.dash.LINK_EVOLUTION, contents + filters)
 
 
     def test_page_comparison(self):
         """ Test the content of page comparison """
 
-        body_elem = ["plot_comp_1", "radio_comp_1", "plot_comp_2", "radio_comp_2"]
-        sidebar_elem = ["drop_comp_categ"] #, "slider_comp_rolling_avg"]
-        self._check_one_page(c.dash.LINK_COMPARISON, body_elem + sidebar_elem)
+        contents = ["plot_comp_1", "radio_comp_1", "plot_comp_2", "radio_comp_2"]
+        filters = ["input_categories", "input_smoothing"]
+        self._check_one_page(c.dash.LINK_COMPARISON, contents + filters)
 
 
     def test_page_heatmaps(self):
         """ Test the content of page heatmaps """
 
-        body_elem = ["plot_heat_i", "plot_heat_e", "plot_heat_distribution"]
-        sidebar_elem = ["drop_heat_categ"]
-        self._check_one_page(c.dash.LINK_HEATMAPS, body_elem + sidebar_elem)
+        contents = ["plot_heat_i", "plot_heat_e", "plot_heat_distribution"]
+        filters = ["input_categories"]
+        self._check_one_page(c.dash.LINK_HEATMAPS, contents + filters)
 
 
     def test_page_pies(self):
         """ Test the content of page pies """
 
-        body_elem = ["drop_pie_1"] # "drop_pie_2", "plot_pie_1", "plot_pie_2"
-        sidebar_elem = ["drop_pie_categ"]
-        self._check_one_page(c.dash.LINK_PIES, body_elem + sidebar_elem)
+        types = [c.names.INCOMES, c.names.EXPENSES]
+        contents = ["plot_pie_{}_{}".format(i, x) for i in range(2) for x in types]
+        contents += ["drop_pie_0", "drop_pie_1"]
+
+        filters = ["input_categories"]
+        self._check_one_page(c.dash.LINK_PIES, contents + filters)
 
 
 
